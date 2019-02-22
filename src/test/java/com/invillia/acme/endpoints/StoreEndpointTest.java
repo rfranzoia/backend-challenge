8be6a07e89049626ed89c9abd6e4578be871c70a
Romeu Franzoia Jr <rfranzoia@gmail.com>
2019-02-21 23:51:53 -0300

    -fixed endpoint testing for store

--- src/test/java/com/invillia/acme/endpoints/StoreEndpointTest.java
+++ src/test/java/com/invillia/acme/endpoints/StoreEndpointTest.java
@@ -15,14 +15,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.invillia.acme.dto.StoreDTO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class StoreEndpointTest
-{
+public class StoreEndpointTest {
 
     private final String localhost = "http://localhost:";
 
@@ -31,28 +31,41 @@ public class StoreEndpointTest
 
     private TestRestTemplate restTemplate = new TestRestTemplate();
 
-
     @Test
-    public void testListStores() throws InterruptedException
-    {
+    public void testListStores() throws InterruptedException {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString());
+	HttpHeaders headers = new HttpHeaders();
+	headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString());
+	headers.add(HttpHeaders.AUTHORIZATION,
+		"Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTU1MTY2MzY1Mn0.keYSIdZUcbZnnG8LHM9sRhvvjArKf40c0BnNX6ewZPLwjgCE84PbW4UG6y0gSb64v3HkM9z6CZAs2XymRXyNPQ");
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(this.localhost + this.port + "/v1/stores/");
+	UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(this.localhost + this.port + "/v1/stores/");
 
-        HttpEntity<?> entity = new HttpEntity<>(headers);
+	HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<StoreDTO[]> response = this.restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, entity, StoreDTO[].class);
-        
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	ResponseEntity<StoreDTO[]> response = this.restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, entity, StoreDTO[].class);
 
-        Thread.sleep(500L);
+	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        StoreDTO[] stores = response.getBody();
+	Thread.sleep(500L);
 
-        assertThat(stores.length).isEqualTo(3);
+	StoreDTO[] stores = response.getBody();
+
+	assertThat(stores.length).isEqualTo(3);
 
     }
-    
+
+    @SuppressWarnings("unused")
+    @Test(expected = RestClientException.class)
+    public void testListCarsAuthenticationFail() throws InterruptedException {
+
+	HttpHeaders headers = new HttpHeaders();
+	headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString());
+
+	UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(this.localhost + this.port + "/v1/stores/");
+
+	ResponseEntity<StoreDTO[]> response = this.restTemplate.getForEntity(builder.build().encode().toUri(), StoreDTO[].class);
+
+    }
+
 }
